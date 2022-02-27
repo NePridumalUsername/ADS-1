191f3c9138988f432cb254db362fcdd1782c2709
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-02-27 15:59:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,41 +5,29 @@
 
 
 bool checkPrime(uint64_t value) {
- if (value % 2 == 0) return 0;
- uint64_t root = sqrt(value);
- for (uint64_t i = 3; i <= root; i += 2)
- {
-if (value % i == 0) return 0;
-}
-return 1;
-}
+if (value % 2 == 0) return 0;
+uint64_t root = sqrt(value);
+for (uint64_t i = 3; i <= root; i += 2) {
+if (value % i == 0) return 0; }
+return 1; }
 
 uint64_t nPrime(uint64_t n) {
 uint64_t i = 0, num = 1;
-while (i < n)
-{
+while (i < n) {
 num++;
-if (checkPrime(num)) i++;
-}
-return num;
-}
+if (checkPrime(num)) i++; }
+return num; }
 
 uint64_t nextPrime(uint64_t value) {
 uint64_t i = 0, num = 1;
-while (i < n)
-{
+while (i < value) {
 num++;
-if (checkPrime(num)) i++;
-}
-return num;
-}
+if (checkPrime(num)) i++; }
+return num; }
 
 uint64_t sumPrime(uint64_t hbound) {
 uint64_t i = 0, num = 1;
-while (i < n)
-{
+while (i < hbound) {
 num++;
-if (checkPrime(num)) i++;
-}
-return num;
-}
+if (checkPrime(num)) i++; }
+return num; }
