8daa6f972305abbb8632fa7fa2e1ea701a75d569
Ivanov Vladimir <99176333+NePridumalUsername@users.noreply.github.com>
2022-02-27 15:17:07 +0300

    VIcode(ADS-1).cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,41 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+if (value % 2 == 0) return 0;
+uint64_t root = sqrt(value);
+for (uint64_t i = 3; i <= root; i += 2)
+{
+if (value % i == 0) return 0;
+}
+return 1;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t i = 0, num = 1;
+while (i < n)
+{
+num++;
+if (checkPrime(num)) i++;
+}
+return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+uint64_t i = 0, num = 1;
+while (i < n)
+{
+num++;
+if (checkPrime(num)) i++;
+}
+return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t i = 0, num = 1;
+while (i < n)
+{
+num++;
+if (checkPrime(num)) i++;
+}
+return num;
 }
